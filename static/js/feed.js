de9c93f69d0dea429ae6c82ec57eb3f28b7ff28a
Jeonghoon <hxxjxxxhxxx@gmail.com>
2022-12-02 20:21:00 +0900

    infinite scroll 수정

--- static/js/feed.js
+++ static/js/feed.js
@@ -1,25 +1,7 @@
-// $(document).ready(function () {
-//     var page = 2;
-//     const $container = get('#container')
-
-//     $(window).scroll(function() {
-//         if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-//         console.log(++page);
-//         $container.append('<article class="feed"></article>');
-        
-//         }
-//     });
-// });
-
-
 window.onscroll = function(ev) {
     let temp_html = `<article class="feed"></article>`
     const {scrollTop, scrollHeight, clientHeight} = document.documentElement
-    if (scrollTop + clientHeight >= scrollHeight - 100) {
-    // if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    //     console.log(window.innerHeight)
-    //     console.log(window.window.scrollY)
-        // console.log(scrollHeight)
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
         $('#container').append(temp_html)
     }
 };
