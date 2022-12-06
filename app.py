78645392439364d4ed0ba9d2cbeba3e298db5036
Lee HyeWon <lulilaks7@gmail.com>
2022-12-06 17:30:38 +0900

    lee 브랜치와 병합했습니다

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, url_for, redirect, flash
 from flask_bcrypt import Bcrypt
 import pymysql, logging
+import json
 
 app = Flask(__name__)
 app.secret_key = 'abcdefg'
@@ -162,44 +163,6 @@ def user_edit():
     else:
         return render_template('user_edit.html')
 
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
-
-
-import pymysql
-import json
-
-
-
-
-@app.route('/')
-def mypage():
-    db = pymysql.connect(host='localhost', user='root', db='mapaltofu', password='xK7C8r9nJF', charset='utf8')
-    curs = db.cursor()
-
-    sql = """
-            select * 
-            from feed as f
-            LEFT JOIN `user` as u
-            ON f.user_id = u.id
-            """
-    curs.execute(sql)
-    rows = curs.fetchall()
-    row = [list(rows[x] for x in range(len(rows)))]
-
-    i_list = []
-    for i in range(len(row)):
-        i_list.insert(0,i)
-
-
-
-    db.commit()
-    db.close()
-    return render_template('mypage.html', feed_list=row, num_list = i_list)
-
 
 
 @app.route("/api/mypages", methods=['GET'])
@@ -222,18 +185,6 @@ def feed_get():
     db.commit()
     db.close()
     return json_str, 200
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
